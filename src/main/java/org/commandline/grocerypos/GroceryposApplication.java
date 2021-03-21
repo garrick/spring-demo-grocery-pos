76fba6fdf6e189bbcd1ba23d6cf24e94aa6df925
Garrick West <garrick@acm.org>
2021-03-20 17:04:58 -0700

    Initial commit for project structure and build

---
+++ src/main/java/org/commandline/grocerypos/GroceryposApplication.java
@@ -0,0 +1,13 @@
+package org.commandline.grocerypos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryposApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/commandline/grocerypos/GroceryposApplicationTests.java
@@ -0,0 +1,13 @@
+package org.commandline.grocerypos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
